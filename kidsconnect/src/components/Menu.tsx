1a839d1723e15f67d3c3e1402e68a6443eab8afc
prakash2812 <pltkval@gmail.com>
2023-05-24 22:40:10 +0530

    comment children menu

--- kidsconnect/src/components/Menu.tsx
+++ kidsconnect/src/components/Menu.tsx
@@ -1,9 +1,12 @@
 import { NavLink } from 'react-router-dom';
+import { SCREENS } from '../constants/appConstants';
+
+const { SESSIONS_PATH, HOME_PATH, CHILDREN_PATH, NEWS_PATH } = SCREENS;
 const menuLists = [
-    { id: crypto.randomUUID(), name: 'Home', to: '/' },
-    { id: crypto.randomUUID(), name: 'Sessions', to: '/sessions' },
-    { id: crypto.randomUUID(), name: 'Childrens', to: '/children' },
-    { id: crypto.randomUUID(), name: 'News', to: '/news' },
+    { id: crypto.randomUUID(), name: 'Home', to: HOME_PATH },
+    { id: crypto.randomUUID(), name: 'Sessions Overview', to: SESSIONS_PATH },
+    // { id: crypto.randomUUID(), name: 'Childrens', to: CHILDREN_PATH },
+    { id: crypto.randomUUID(), name: 'News', to: NEWS_PATH },
 ];
 const Menu = () => {
     return (
