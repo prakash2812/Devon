5b6d68b5ef7fc1c4107a5627372b669c8ea93099
prakash2812 <pltkval@gmail.com>
2023-05-25 03:50:58 +0530

    Modified cards and its backgrounds

--- kidsconnect/src/components/SessionItem/SessionItem.tsx
+++ kidsconnect/src/components/SessionItem/SessionItem.tsx
@@ -1,39 +1,58 @@
 import { memo } from 'react';
-import { Card, CardContent, Typography, Button } from '@mui/material';
+import { Card, CardContent, Typography, Button, Avatar } from '@mui/material';
 import { SessionItemProps } from '../../Interface/SessionInterface';
 
 const SessionItem = memo(({ session, handleStatusChange }: SessionItemProps) => {
     const {
         presence,
         id,
+        name: childname,
+        avatar,
         start_time,
         child_id,
         end_time,
         group: { name },
     } = session;
 
+    const getStatusClass = (presence: string) => {
+        if (presence === 'unknown') {
+            return 'unknown-status';
+        } else if (presence === 'picked up') {
+            return 'picked-up-status';
+        } else {
+            return 'present-status';
+        }
+    };
     return (
-        <Card>
+        <Card className={`session-card ${getStatusClass(presence)}`}>
             <CardContent>
+                <Avatar src={avatar} alt="Avatar" sx={{ width: 40, height: 40 }} />
+
                 <Typography variant="h5">{`${start_time} - ${end_time}`}</Typography>
-                <Typography className="session-details">
-                    <p>
+                <div className="session-details">
+                    <div>
                         <em>Child ID</em>
-                    </p>
-                    <p>{child_id}</p>
-                </Typography>
-                <Typography className="session-details">
-                    <p>
+                    </div>
+                    <div>{child_id}</div>
+                </div>
+                <div className="session-details">
+                    <div>
+                        <em>Child Name</em>
+                    </div>
+                    <div>{childname}</div>
+                </div>
+                <div className="session-details">
+                    <div>
                         <em>Group</em>
-                    </p>
-                    <p>{name}</p>
-                </Typography>
-                <Typography className="session-details">
-                    <p>
+                    </div>
+                    <div>{name}</div>
+                </div>
+                <div className="session-details">
+                    <div>
                         <em>Status</em>
-                    </p>
-                    <p>{presence}</p>
-                </Typography>
+                    </div>
+                    <div>{presence}</div>
+                </div>
                 <Button variant="contained" onClick={() => handleStatusChange(id, presence)}>
                     Update Status
                 </Button>
